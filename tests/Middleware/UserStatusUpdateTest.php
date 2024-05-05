ada72ef79445ba06e01cae4a5936498c3fd3ba85
brianclogan <brianclogan@users.noreply.github.com>
2024-05-05 21:15:13 +0000

    Fix styling

--- tests/Middleware/UserStatusUpdateTest.php
+++ tests/Middleware/UserStatusUpdateTest.php
@@ -10,12 +10,12 @@ beforeAll(function () {
 // TODO:: Make sure this test is functioning, currently it is not
 it('sets the status to online on a visit', function () {
     expect(true)->tobeTrue();
-//    $user = \Workbench\App\Models\User::factory(1)->create()->first();
-//
-//    $this->actingAs($user)
-//        ->get('/')
-//        ->assertOk();
-//
-//    ray($user->status);
+    //    $user = \Workbench\App\Models\User::factory(1)->create()->first();
+    //
+    //    $this->actingAs($user)
+    //        ->get('/')
+    //        ->assertOk();
+    //
+    //    ray($user->status);
     //    expect($user->status)->toBe('online');
 });
